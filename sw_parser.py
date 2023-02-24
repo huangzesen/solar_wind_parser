9d3196a3f3c8868e1e7e40b7159dd4027160b069
huangzesen <hzsbazinga@outlook.com>
2023-02-24 11:12:51 -0800

    Update sw_parser.py

--- sw_parser.py
+++ sw_parser.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 import time, sys
-from scipy.stats import shapiro
+from scipy.stats import shapiro, kstest
+from scipy.interpolate import interp1d
 from multiprocessing import Pool
+import tqdm
+from gc import collect
 
 # default settings
 default_settings = {
@@ -43,10 +46,270 @@ default_settings = {
 }
 
 
+
+def SolarWindScanner(
+    Btot,
+    Dist_au,
+    settings = None,
+    parallel = False,
+    verbose = False,
+    Ncores = 8
+):
+
+    """
+    Solar Wind Scanner
+    Input:
+    """
+
+    if 'methods' not in settings.keys():
+        settings['methods'] = {
+            'shapiro': {'Ntests': 2000},
+            'kstest': {'Ntests': 600}
+        }
+    
+    if 'downsample_size' not in settings.keys():
+        settings['downsample_size'] = 500
+
+    if verbose:
+        print("\n----------------------------------")
+        print("\tSolar Wind Scanner")
+        print("----------------------------------\n")
+        print("Settings: ")
+        for k,v in settings.items():
+            print(k,v)
+
+    win = settings['win']
+    step = settings['step']
+
+    tstart0 = Btot.index[0]
+    tend0 = tstart0 + win
+    scans = []
+
+    # calculate num of steps
+    N = int(np.floor((Btot.index[-1] - tend0)/step)) + 1
+    inputs = []
+
+    # show length of Btot and Dist_au
+    if len(Btot) != len(Dist_au):
+        # print("\nlen(Btot)=%d, len(Dist_au)=%d, interpolate Dist_au!" %(len(Btot), len(Dist_au)))
+        print('Interpolating Dist_au...')
+
+        # interpolate Dist_au with scipy.interpolate.interp1d
+        ts = [t.timestamp() for t in Dist_au.index]
+        f_dist_au = interp1d(ts, Dist_au.values, 'linear', fill_value='extrapolate')
+
+        # create Btot unix index
+        Btot_index_unix = [t.timestamp for t in Btot.index]
+
+        # print the input value range and Btot index range
+        print('Dist_au: %s - %s' %(Dist_au.index[0], Dist_au.index[-1]))
+        print('Btot: %s - %s' %(Btot.index[0], Btot.index[-1]))
+
+    print("\nTotal intervals= %d\n" %(N))
+
+    if parallel:
+        print("Mode: parallel")
+
+        alloc_input = {
+            'Btot': Btot,
+            'Dist_au': Dist_au,
+            'f_dist_au': f_dist_au,
+            'Btot_index_unix': Btot_index_unix,
+            'tstart0': tstart0,
+            'settings': settings
+        }
+        
+        with Pool(Ncores, initializer=InitParallelAllocation, initargs=(alloc_input,)) as p:
+            for scan in tqdm.tqdm(p.imap_unordered(SolarWindScannerInnerLoopParallel, range(N)), total=N):
+                scans.append(scan)
+
+
+    else:
+
+        print("Mode: sequential")
+        for i1 in range(N):
+            tstart= tstart0 + i1 * step
+            tend = tstart + win
+            ind = (Btot.index >= tstart) & (Btot.index < tend)
+            btot = Btot[ind]
+            r = Dist_au[ind]
+            input = {
+                'tstart': tstart,
+                'tend': tend,
+                'btot': btot,
+                'r': r
+            }
+            inputs.append(input)
+
+            print("%d out of %d allocated, %.2f%% finished" %(i1, N, i1/N*100), end='\r')
+        
+        print("Finished! Occupying %.2f MB, Start scanning...\n" %(sys.getsizeof(inputs)/1024.))
+
+        # print("Scanning Mode: Sequential")
+        for i1 in range(N):
+            scan = SolarWindScannerInnerLoopSequential(inputs[i1])
+            scans.append(scan)
+
+            if verbose:
+                print(r"scanning...: tstart=%s, tend=%s, normality=%.4f, finished = %.2f%%" %(
+                    scan['t0'], 
+                    scan['t1'], 
+                    scan['normality'], 
+                    (scan['t1']-Btot.index[0])/(Btot.index[-1]-Btot.index[0])*100
+                ), end='\r')
+
+    return scans
+
+
+def InitParallelAllocation(alloc_input):
+    global Btot, Dist_au, tstart0, settings, f_dist_au, Btot_index_unix
+    Btot = alloc_input['Btot'].copy()
+    Dist_au = alloc_input['Dist_au'].copy()
+    tstart0 = alloc_input['tstart0']
+    settings = alloc_input['settings']
+    f_dist_au = alloc_input['f_dist_au']
+    Btot_index_unix = alloc_input['Btot_index_unix']
+
+
+def SolarWindScannerInnerLoopParallel(i1):
+    # access global variables
+    global Btot, Dist_au, tstart0, settings, f_dist_au, Btot_index_unix
+
+    win = settings['win']
+    step = settings['step']
+    methods = settings['methods']
+    downsample_size = settings['downsample_size']
+
+    tstart= tstart0 + i1 * step
+    tend = tstart + win
+    ind = (Btot.index >= tstart) & (Btot.index < tend)
+    indices = Btot.index[ind]
+    btot = Btot[indices].values
+
+    if len(Btot) == len(Dist_au):
+        # if same sampling frequency
+        r = Dist_au[indices].values
+    else:
+        # use interpolated f_dist_au
+        ts = Btot_index_unix[ind]
+        r = f_dist_au(ts)
+
+        # # linear rescale
+        # id1 = np.argmin(np.abs(Dist_au.index  - tstart))
+        # id2 = np.argmin(np.abs(Dist_au.index  - tend))
+        # r1 = Dist_au[id1]
+        # r2 = Dist_au[id2]
+        # r = np.linspace(r1, r2, len(btot))
+        
+    # normalize btot with r
+    btot1 = btot * ((r/r[0])**2)
+
+    # dist ratio
+    r_ratio = np.max(r)/np.min(r)
+    
+    
+    # discard points outside of 3 sigma
+    # solar wind has a much higher chance than gaussian to have extreme values
+    mean = np.mean(btot1)
+    std = np.std(btot1)
+    keep_ind = (btot1 > mean - 3*std) & (btot1 < mean + 3*std)
+    btot1[np.invert(keep_ind)] = np.nan
+    nan_ratio = np.sum(np.isnan(btot1))/len(btot1)
+    x = btot1[np.invert(np.isnan(btot1))]
+
+    normalities = {}    
+    for method, ms in methods.items():
+        if len(x) < 500:
+            # if not enough samples, continue
+            normality = np.nan
+        else:
+            # normalize input for normality test
+            x = (x-np.mean(x))/np.std(x)
+
+            # normality test
+            if method == 'shapiro':
+                a1 = np.array([shapiro(np.random.choice(x, size=downsample_size, replace = False)).pvalue for i1 in range(ms['Ntests'])])
+            elif method == 'kstest':
+                # normalize the distribution for kstest
+                a1 = np.array([kstest(np.random.choice(x, size=downsample_size, replace = False), 'norm').pvalue for i1 in range(ms['Ntests'])])
+            else:
+                raise ValueError("Wrong method")
+            normality = np.sum(a1 > 0.05)/len(a1)
+        
+        normalities[method] = normality
+    
+    scan = {
+        't0': tstart,
+        't1': tend,
+        'nan_ratio': nan_ratio,
+        'normalities': normalities,
+        'r_ratio': r_ratio
+    }
+
+    return scan
+
+
+def SolarWindScannerInnerLoopSequential(input):
+    # allocate variables
+    btot = input['btot']
+    r = input['r']
+    tstart = input['tstart']
+    tend = input['tend']
+
+    # normalize btot with r
+    btot1 = btot * ((r/r[0])**2)
+    
+    # discard points outside of 3 sigma
+    # solar wind has a much higher chance than gaussian to have extreme values
+    mean = np.mean(btot1)
+    std = np.std(btot1)
+    keep_ind = (btot1 > mean - 3*std) & (btot1 < mean + 3*std)
+    btot1[np.invert(keep_ind)] = np.nan
+    x = btot1.values[np.invert(np.isnan(btot1))]
+
+    if len(x) < 500:
+        # if not enough samples, continue
+        normality = np.nan
+    else:
+        # normality test
+        a1 = np.array([shapiro(np.random.choice(x, size=500)).pvalue for i1 in range(10000)])
+        normality = np.sum(a1 > 0.05)/len(a1)
+    
+    scan = {
+        't0': tstart,
+        't1': tend,
+        'normality': normality
+    }
+
+    return scan
+
+
+# some testing function for parallelization
+
+# def testing(x, parallel = False):
+
+#     if not parallel:
+#         a1 = np.array([shapiro(np.random.choice(x, size=500)).pvalue for i1 in range(1000000)])
+#     else:
+#         with Pool(4) as p:
+#             a1 = p.map(f, [x for i1 in range(1000000)])
+#             a1 = np.array(a1)
+
+#     return a1
+
+# def f(x):
+#     return shapiro(np.random.choice(x, size=500)).pvalue
+
+
+
+
+
 def SolarWindParser(
         Btot, 
         Dist_au,
-        settings=None
+        settings=None,
+        parallel=False,
+        method = 'shapiro'
     ):
     """
     Solar Wind Parser
@@ -110,20 +373,27 @@ def SolarWindParser(
         btot1[np.invert(keep_ind)] = np.nan
         # drop the nans, note that interpolate is wrong, which is enhancing normality
         x = btot1.values[np.invert(np.isnan(btot1))]
+        nan_ratio = np.sum(np.isnan(btot1))/len(btot1)
 
         # if all nan
         if len(x) < 500:
             normality = np.nan
         else:
             # normality test
-            a1 = np.array([shapiro(np.random.choice(x, size=500)).pvalue for i1 in range(10000)])
+            if method == 'shapiro':
+                a1 = np.array([shapiro(np.random.choice(x, size=500, replace = False)).pvalue for i1 in range(10000)])
+            elif method == 'kstest':
+                a1 = np.array([kstest(np.random.choice(x, size=500, replace = False), 'norm').pvalue for i1 in range(3000)])
+            else:
+                raise ValueError("Wrong method")
             normality = np.sum(a1 > 0.05)/len(a1)
         
         # append scan
         scan = {
             't0': tstart,
             't1': tend,
-            'normality': normality
+            'normality': normality,
+            'nan_ratio': nan_ratio
         }
         scans.append(scan)
             
@@ -165,15 +435,23 @@ def SolarWindParser(
                 btot1[np.invert(keep_ind)] = np.nan
                 # drop the nans, note that interpolate is wrong, which is enhancing normality
                 x = btot1.values[np.invert(np.isnan(btot1))]
+                nan_ratio_in = np.sum(np.isnan(btot1))/len(btot1)
                 
                 # normality test
-                a2 = np.array([shapiro(np.random.choice(x, size=500)).pvalue for i1 in range(10000)])
+                if method == 'shapiro':
+                    a2 = np.array([shapiro(np.random.choice(x, size=500, replace = False)).pvalue for i1 in range(10000)])
+                elif method == 'kstest':
+                    a2 = np.array([kstest(np.random.choice(x, size=500, replace = False), 'norm').pvalue for i1 in range(10000)])
+                else:
+                    raise ValueError("Wrong method")
+                normality = np.sum(a1 > 0.05)/len(a1)
 
                 normality_in = np.sum(a2 > 0.05)/len(a2)
                 scan_in = {
                     't0': tstart_in,
                     't1': tend_in,
-                    'normality': normality_in
+                    'normality': normality_in,
+                    'nan_ratio': nan_ratio_in
                 }
                 
                 # append the scan_in
@@ -198,8 +476,9 @@ def SolarWindParser(
                         # if the last one is good, break
                         else:
                             print("")
-                            print("keeping: tstart=%s, tend=%s, normality=%.4f, len = %.2f Hr" %(
-                                scans_in[-1]['t0'], scans_in[-1]['t1'], scans_in[-1]['normality'], (scans_in[-1]['t1']-scans_in[-1]['t0'])/np.timedelta64(1, 'h')
+                            print("keeping: tstart=%s, tend=%s, normality=%.4f, nan_ratio=%.4f, len = %.2f Hr" %(
+                                scans_in[-1]['t0'], scans_in[-1]['t1'], scans_in[-1]['normality'], scans_in[-1]['nan_ratio'], 
+                                (scans_in[-1]['t1']-scans_in[-1]['t0'])/np.timedelta64(1, 'h')
                                 ))
                             # set the new starting point
                             print("")
@@ -280,7 +559,7 @@ def SolarWindParser(
                     break
                 # otherwise eat one more step
                 else:
-                    print("Eating...: tstart=%s, tend=%s, normality=%.4f, len = %s" %(tstart_in, tend_in, normality_in, tend_in - tstart_in))
+                    print("Eating...: tstart=%s, tend=%s, normality=%.4f, nan_ratio = %.4f, len = %s" %(tstart_in, tend_in, normality_in, nan_ratio_in, tend_in - tstart_in))
                     tend_in = tend_in + step_in    
                     
                 # end the loop if it reach the end
@@ -295,14 +574,15 @@ def SolarWindParser(
             tspent = (time.time() - tl0)
             ratio = (tend-Btot.index[0])/(Btot.index[-1]-Btot.index[0])
             # clear_output(wait=True)
-            print(r"scanning...: tstart=%s, tend=%s, normality=%.4f, finished = %.2f%%, time spent = %.2f min, time left = %.2f min" %(
+            print("scanning...: tstart=%s, tend=%s, normality=%.4f, nan_ratio=%.4f, finished = %.2f%%, time spent = %.2f min, time left = %.2f min" %(
                 tstart, 
                 tend, 
                 normality, 
+                nan_ratio,
                 (tend-Btot.index[0])/(Btot.index[-1]-Btot.index[0])*100,
                 tspent/60,
                 (tspent/ratio-tspent)/60
-                ))
+                ), end='\r')
             tstart = tstart + step_out
             tend = tend + step_out
         
@@ -317,103 +597,3 @@ def SolarWindParser(
 
     return results
 
-
-def SolarWindScanner(
-    Btot,
-    Dist_au,
-    settings = None,
-    parallel = False,
-    verbose = False,
-    Ncores = 8
-):
-
-    """
-    Solar Wind Scanner
-    Input:
-    """
-
-    # settings
-    if settings is None:
-        settings = default_settings
-
-    for k,v in settings.items():
-        print(k,v)
-
-    win = settings['win']
-    step_out = settings['step_out']
-
-    tstart0 = Btot.index[0]
-    tend0 = tstart0 + win
-    scans = []
-
-    # calculate num of steps
-    N = int(np.floor((Btot.index[-1] - tend0)/step_out)) + 1
-    inputs = []
-
-    print("\nAllocating inputs... Total = %d" %(N))
-    for i1 in range(N):
-        tstart= tstart0 + i1 * step_out
-        tend = tstart + win
-        ind = (Btot.index >= tstart) & (Btot.index < tend)
-        btot = Btot[ind]
-        r = Dist_au[ind]
-        input = {
-            'tstart': tstart,
-            'tend': tend,
-            'btot': btot,
-            'r': r
-        }
-        inputs.append(input)
-    print("Finished! Occupying %.2f MB, Start scanning..." %(sys.getsizeof(inputs)/1024.))
-
-    if not parallel:
-        print("Mode: Sequential")
-        for i1 in range(N):
-            scan = SolarWindScannerInnerLoop(inputs[i1])
-            scans.append(scan)
-
-            if verbose:
-                print(r"scanning...: tstart=%s, tend=%s, normality=%.4f, finished = %.2f%%" %(
-                    scan['t0'], 
-                    scan['t1'], 
-                    scan['normality'], 
-                    (scan['t1']-Btot.index[0])/(Btot.index[-1]-Btot.index[0])*100
-                ))
-    else:
-        print("Mode: Parallel")
-        with Pool(Ncores) as p:
-            scans = p.map(
-                SolarWindScannerInnerLoop, inputs
-            )
-
-    return scans
-
-def SolarWindScannerInnerLoop(input):
-    # allocate variables
-    btot = input['btot']
-    r = input['r']
-    tstart = input['tstart']
-    tend = input['tend']
-
-    # normalize btot with r
-    btot1 = btot * ((r/r[0])**2)
-    
-    # discard points outside of 3 sigma
-    # solar wind has a much higher chance than gaussian to have extreme values
-    mean = np.mean(btot1)
-    std = np.std(btot1)
-    keep_ind = (btot1 > mean - 3*std) & (btot1 < mean + 3*std)
-    btot1[np.invert(keep_ind)] = np.nan
-    x = btot1.values[np.invert(np.isnan(btot1))]
-    
-    # normality test
-    a1 = np.array([shapiro(np.random.choice(x, size=500)).pvalue for i1 in range(10000)])
-    
-    normality = np.sum(a1 > 0.05)/len(a1)
-    scan = {
-        't0': tstart,
-        't1': tend,
-        'normality': normality
-    }
-
-    return scan
